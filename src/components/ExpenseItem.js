f0aa8d4d0539d397bb54ef24022245e7b81f56d0
Leon <leon@News-MBP.fritz.box>
2022-07-09 20:48:27 +0200

    first react app basic setup with first card components

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css'
+import Card from './Card';
+
+function ExpenseItem(props) {
+
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'><h2>{props.title}</h2></div>
+            <div className='expense-item__price'><h2>{props.amount}$</h2></div>
+        </Card>
+    )
+}
+
+export default ExpenseItem;
